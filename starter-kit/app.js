ccc590511cfcfa18525d1e719e40842839348323
Tanner Hodges <austintannerhodges@gmail.com>
2017-12-06 15:55:07 -0500

    Update starter-kit

--- starter-kit/app.js
+++ starter-kit/app.js
@@ -1,9 +1,55 @@
-// Shims + Polyfills
+// Polyfills
+import 'core-js/fn/array/filter';
 import 'core-js/fn/array/from';
+import 'core-js/fn/array/index-of';
+import 'core-js/fn/array/is-array';
+import 'core-js/fn/array/map';
+import 'core-js/fn/array/reduce';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/object/entries';
 
-// Browser Detects
+// Animation
+import slideUp from './animation/slideUp';
+import slideDown from './animation/slideDown';
+
+// Browser Detect
+import chrome from './browserDetect/chrome';
+import fontFeatures from './browserDetect/fontFeatures';
+import hasTouched from './browserDetect/hasTouched';
 import internetExplorer from './browserDetect/internetExplorer';
 import ios from './browserDetect/ios';
-import fontFeatures from './browserDetect/fontFeatures';
 
-console.log('Hello!');
+// DOM
+import childDepth from './dom/childDepth';
+import children from './dom/children';
+import classList from './dom/classList';
+import closest from './dom/closest';
+import getIndex from './dom/getIndex';
+import getWindow from './dom/getWindow';
+import intersectionRatio from './dom/intersectionRatio';
+import isInBounds from './dom/isInBounds';
+import isInViewport from './dom/isInViewport';
+import isVisible from './dom/isVisible';
+import matches from './dom/matches';
+import offset from './dom/offset';
+import outerHeight from './dom/outerHeight';
+import outerWidth from './dom/outerWidth';
+import parents from './dom/parents';
+import position from './dom/position';
+import ResizeSensor from './dom/ResizeSensor';
+import Visibility from './dom/Visibility';
+
+// Event
+import animationEvent from './event/animationEvent';
+import clearStack from './event/clearStack';
+import customEvent from './event/customEvent';
+import debounce from './event/debounce';
+import on from './event/on';
+import throttle from './event/throttle';
+import transitionEvent from './event/transitionEvent';
+
+// Time
+import daysSince from './time/daysSince';
+
+// Typography
+import shorten from './time/shorten';
